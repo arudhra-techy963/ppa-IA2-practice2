702a97e1af5094bd819068686b8f176175476639
Arudhra Chandrasekaran <94979081+arudhra-techy963@users.noreply.github.com>
2022-03-29 03:18:02 +0000

    p6 reversing a string

---
+++ p6original.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+void input_string(char *a);
+int str_reverse(char *a);
+void output(char *a,char reversea);
+
+int main()
+{
+  char a[100];
+  input_string(a);
+  int reverse_a;
+  reverse_a=str_reverse(a);
+  output(a,reverse_a);
+  return 0;
+}
+
+void input_string(char *a)
+{
+  printf("Enter the string: \n");
+  scanf("%s",a);
+}
+
+
+int str_reverse(char *a)
+{
+  int i,m=0;
+  for(i=0;a[i]!='\0';i++)
+  m++;
+  return m;
+}
+
+
+void output(char *a,char reversea)
+{
+  printf("The reverse of the string is: \n");
+  
+  for(int i=reversea-1;a[i]!='\0';i--)
+  {
+    printf("%c",a[i]);
+  }
+  printf("\n");
+}
+
